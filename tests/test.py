c358cc5cc715d8c593de91c50e6a24fc9c958beb
Mitchell-Gerrard <mitchell.gerrard99@hotmail.com>
2025-07-02 03:13:17 +0100

    Add video fractal generation to test and include test video files

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,8 @@ def test_fractal_media():
     image_fractal.generate_square_root_fractal(num_iterations=2, shift=-0.70176-0.3842j,scale=1)
     image_fractal.generate_square_root_fractal(num_iterations=2, shift=-0.2-0.3j,scale=1)
     image_fractal.save_fractal('tests/test_fractal.jpg',dpi=300)
+    video_fractal = fractal_media.fractal_media.video_fractal()
+    video_fractal.generate_fractal(input_video='tests/test_video.mp4', output_video='tests/test_out.mp4', num_iterations=2, shift=-0.4-0.3842j, scale=1, flip=1)
 
 
 
